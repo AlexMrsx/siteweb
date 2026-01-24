beb24603c0adb91430849539c88b37857bb9e8b4
AlexMrsx <alex.marescaux62@gmail.com>
2026-01-24 14:32:10 +0100

    Ajout Capitalum

---
+++ script/dark-mode.js
@@ -0,0 +1,28 @@
+document.querySelector(".darkmode").addEventListener("click", () => {
+    console.log("Dark mode toggled");
+  document.documentElement.classList.toggle("dark");
+    if (document.documentElement.classList.contains("dark")) {
+        whileblackcolor();
+    } else {
+        document.querySelectorAll("img").forEach((img) => {
+            img.style.filter = "none";
+        });
+    }
+    document.querySelectorAll("div a").forEach((link) => {
+        link.addEventListener("mouseover", () => {
+            if (document.documentElement.classList.contains("dark")) {
+                link.style.color = "white";
+            } else {
+                link.style.color = "black";
+            }
+
+    });
+});
+});
+
+
+function whileblackcolor()  {
+    document.querySelectorAll("img").forEach((img) => {
+        img.style.filter = "grayscale(1)";
+    });
+}
